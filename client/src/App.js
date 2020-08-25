1aab48753c5c84ebf221ab75d8b88249009406e5
IwanJones41299 <iwanjones41299@gmail.com>
2020-08-25 17:38:57 +0100

    Login & register routes completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from './Components/Navbar';
+import Login from "./Components/Login";
 import Home from "./Components/Home";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from "./Components/Register";
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Route exact path = "/" component={Home} />
+      <Route path = "/login" component={Login} />
+      <Route path = "/register" component={Register} />
     </Router>
   );
 }
--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -1,37 +1,43 @@
 export default {
-    login : user => {
-        return fetch('/user/login', {
+    login : user =>{
+        console.log(user);
+        return fetch('/user/login',{
+            method : "post",
+            body : JSON.stringify(user),
+            headers : {
+                'Content-Type' : 'application/json'
+            }
+        }).then(res => {
+            if(res.status !== 401)
+                return res.json().then(data => data);
+            else
+                return { isAuthenticated : false, user : {username : "",role : ""}};
+        })
+    },
+    register : user =>{
+        console.log(user);
+        return fetch('/user/register',{
             method : "post",
             body : JSON.stringify(user),
             headers : {
                 'Content-Type' : 'application/json'
             }
         }).then(res => res.json())
-            .then(data => data);
+          .then(data => data);
     },
-    register : user => {
-        return fetch('/user/register', {
-            method : "post",
-            body : JSON.stringify(user),
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        }).then(res => res.json())
-            .then(data => data);
-    },
-    logout : () => {
+    logout : ()=>{
         return fetch('/user/logout')
                 .then(res => res.json())
                 .then(data => data);
     },
-    isAuthenticated : () => {
+    isAuthenticated : ()=>{
         return fetch('/user/authenticated')
-            .then(res => {
-                if(res.status !== 401)
-                    return res.json().then(data => data);
-                else
-                    return { isAuthenticated : false, user : {username : "", role : ""}};
-            });
+                .then(res=>{
+                    if(res.status !== 401)
+                        return res.json().then(data => data);
+                    else
+                        return { isAuthenticated : false, user : {username : "",role : ""}};
+                });
     }
 
 }
