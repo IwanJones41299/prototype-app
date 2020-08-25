e9818acf220365b2a71dc64764561b3409134874
IwanJones41299 <iwanjones41299@gmail.com>
2020-08-25 15:07:55 +0100

    Navbar component completed

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import AuthProvider from './Content/AuthContext';
+import AuthProvider from './Context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render( <AuthProvider> <App /> </AuthProvider>, document.getElementById('root'));
