ef80d60cf7ca223cdd37634cd0280c57aa6435bd
IwanJones41299 <iwanjones41299@gmail.com>
2020-08-26 13:26:06 +0100

    Todo section completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import Navbar from './Components/Navbar';
 import Login from "./Components/Login";
 import Home from "./Components/Home";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Todos from './Components/Todos';
 import Register from "./Components/Register";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route exact path = "/" component={Home} />
       <Route path = "/login" component={Login} />
       <Route path = "/register" component={Register} />
+      <Route path = "/todos" component={Todos} />
     </Router>
   );
 }
